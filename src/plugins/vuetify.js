f1c5a0c6ae6758aef6583a4e5800c7534d19444b
mohammad ashrafi <mohammad.ashrafi2020@outlook.com>
2022-04-29 19:20:23 +0430

    Init Project :)

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,13 @@
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Vuetify
+import {createVuetify} from 'vuetify'
+
+export default createVuetify(
+    // {
+    //     rtl: true
+    // }
+    // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+)
